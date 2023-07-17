4b2e6e91f560d17051cbdee4a84545006d720276
Musa Azeem <musa.mazeem@gmail.com>
2023-07-17 14:30:49 -0400

    script

--- scripts/test-evaluation.sh
+++ scripts/test-evaluation.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Run without seperating sessions
-./scripts/main_nursingv1-mix-sessions.py --n-sessions 40 --shuffle --epochs 100 --dev-size .5
+./scripts/main_nursingv1-mix-sessions.py --n-sessions 40 --shuffle --epochs 100 --dev-size .5 --project nursingv1_projects/evaluation_tests
 
 # Run with seperating sessions
-./scripts/main_nursingv1-seperate-sessions.py --n-sessions 40 --shuffle --epochs 100 --dev-size .5
+./scripts/main_nursingv1-seperate-sessions.py --n-sessions 40 --shuffle --epochs 100 --dev-size .5 --project nursingv1_projects/evaluation_tests
